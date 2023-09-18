1fc63de81300ea7d103e3bb53f505011e45c4b63
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-09-18 18:03:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es el ejercicio 10 correspondiente al capitulo 4
---
+++ src/main/java/john/ejercicio10cap4poo/Ejercicio10cap4Poo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio10cap4poo;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio10cap4Poo {
+
+    public static void main(String[] args) {
+	    Form form = new Form();
+	    form.setVisible(true);
+    }
+}
